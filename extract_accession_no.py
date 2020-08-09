579763935af3d1b53f0901b6018abae4b691850b
Rajan <rajan22091998@gmail.com>
2020-08-09 17:45:48 +0530

    Add files via upload

---
+++ extract_accession_no.py
@@ -0,0 +1,7 @@
+import sys
+f_file = sys.argv[1]
+mlti_fasta = open(f_file, "r")
+acc_no = open("accession_no.txt", "w+")
+for line in mlti_fasta:
+	if line[0] == ">":
+		acc_no.write(line[1:10] + "\n")
