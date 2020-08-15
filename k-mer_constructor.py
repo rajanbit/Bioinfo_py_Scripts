0e2370f1b9060ff6459d119a36082439e94a35f2
Rajan <rajan22091998@gmail.com>
2020-08-15 11:02:53 +0530

    Add files via upload

---
+++ k-mer_constructor.py
@@ -0,0 +1,13 @@
+import sys
+DNA_file = sys.argv[1]
+k_mer_size = input("Enter the K-mer length: ")
+DNA = open(DNA_file, "r")
+seq = ""
+for line in DNA:
+	if line.startswith(">"):
+		pass
+	else:
+		seq += line.replace('\n', "")
+x = int(k_mer_size)
+for i in range(len(seq) - x):
+	print(seq[i:i+int(k_mer_size)])
