0e4f30dc3aac45e7639456f9637ebbeb9fd482a4
Rajan <rajan22091998@gmail.com>
2022-06-05 18:43:20 +0530

    Update seq_analysis.py

--- sequence_analysis/seq_analysis.py
+++ sequence_analysis/seq_analysis.py
@@ -16,3 +16,23 @@ def nucl_comp(seq):
 	plt.savefig('nucleotide_composition.png',bbox_inches ="tight", pad_inches = 0.5)
 	plt.close()
   
+# Function to return GC composition
+def gc_comp(seq):
+	gc_perc = GC(seq)
+	return("GC Percent:", round(gc_perc, 2))
+
+# Function to plot codon composition
+def codon_comp(seq):
+	codons = [seq[x:x+3] for x in range(0, len(seq)+1, 3)]
+	codon_dict = dict.fromkeys(list(set(codons)), 0)
+	for codon in codon_dict:
+		codon_dict[codon] = (codons.count(codon)/len(codons))*100
+	codon_dict={key:value for key,value in codon_dict.items() if len(key)==3}
+	plt.figure(figsize=(10,4))
+	plt.bar(list(codon_dict.keys()), list(codon_dict.values()), color=get_cmap("Accent").colors)
+	plt.xticks(fontsize=8, rotation=90)
+	plt.xlabel("Codons")
+	plt.ylabel("Percentage (%)")
+	plt.title("Codons Composition Plot")
+	plt.savefig('codon_composition.png',bbox_inches ="tight", pad_inches = 0.5, dpi = 500)
+	plt.close()
